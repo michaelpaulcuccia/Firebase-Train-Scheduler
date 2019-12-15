582c722d9a1d6b72fe10174c680a9e34ca6ff85c
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2019-12-15 07:39:43 -0500

    clean-up

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,11 +1,3 @@
-//FROM TIMESHEET LOGIC
-// 1. Initialize Firebase
-// 2. Create button for adding new TRAINS - then update the html + update the database
-// 3. Create a way to retrieve TRAINS from the employee database.
-// 4. Create a way to calculate the "months worked". Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total "billed"
-
 // 1. Initialize Firebase
 var config = {
     apiKey: "AIzaSyCcKCuRm3QBdTqkrOk7nkWrTll6aP7CJlo",
